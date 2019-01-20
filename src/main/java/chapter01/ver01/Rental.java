7aab91e186441fe92da25d38fa2a20884fbcc28d
androidmalin <malin.myemail@163.com>
2019-01-20 18:38:31 +0800

    format all java file

--- src/main/java/chapter01/ver01/Rental.java
+++ src/main/java/chapter01/ver01/Rental.java
@@ -2,19 +2,19 @@ package chapter01.ver01;
 
 //租赁
 public class Rental {
-	private Movie _movie; // 影片
-	private int _daysRented; // 租期
+    private Movie _movie; // 影片
+    private int _daysRented; // 租期
 
-	public Rental(Movie movie, int daysRented) {
-		_movie = movie;
-		_daysRented = daysRented;
-	}
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
 
-	public int getDaysRented() {
-		return _daysRented;
-	}
+    public int getDaysRented() {
+        return _daysRented;
+    }
 
-	public Movie getMovie() {
-		return _movie;
-	}
+    public Movie getMovie() {
+        return _movie;
+    }
 }
